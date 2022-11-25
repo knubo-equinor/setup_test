690f7dc135be8f1b9a8885927d512cd1c6f9322d
Knut Arne Børresen <78740088+knubo-equinor@users.noreply.github.com>
2022-11-25 07:34:38 +0100

    Create setup.py
    setting up packages to be installed

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIREMENTS = [
+    "numpy",
+    "pandas",
+    "requests",
+    "pymc", 
+    "arviz"
+]
+
+setuptools.setup(
+    name="pymc environment",
+    version="0.0.0",
+    author="Knut Arne Borresen",
+    author_email="knubo@equinor.com",
+    description="Setting up for pymc analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+    ],
+    python_requires='>=3.8',
+    install_requires=REQUIREMENTS,
+)
